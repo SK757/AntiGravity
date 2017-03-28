23b73073fbc5050a5bf5bd43032383d3224f8dbe
Sean Kirk <sean.kirk@go.edgehill.ac.uk>
2017-03-28 12:40:11 +0100

    Mobile version complete with accordion menu implemented that includes product information

--- js/dev/javascript.js
+++ js/dev/javascript.js
@@ -1,13 +1,35 @@
 $('#toggle1').click(function() {
-   $('#toggle1').toggleClass('active');
-   $('#toggle2').removeClass('active');
-   $('#sport-overlay').removeClass('open');
+   $('#toggle1').toggleClass('navActive');
+   $('#toggle2').removeClass('navActive');
    $('#overlay').toggleClass('open');
+   $('#sport-overlay').removeClass('open');
 });
 
 $('#toggle2').click(function() {
-   $('#toggle2').toggleClass('active');
-   $('#toggle1').removeClass('active');
+   $('#toggle2').toggleClass('navActive');
+   $('#toggle1').removeClass('navActive');
    $('#overlay').removeClass('open');
    $('#sport-overlay').toggleClass('open');
 });
+
+/******** Accordion ********/
+// https://www.w3schools.com/howto/howto_js_accordion.asp
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+    		panel.style.maxHeight = null;
+    	} else {
+    		panel.style.maxHeight = panel.scrollHeight + "px";
+    	}
+    }
+}
